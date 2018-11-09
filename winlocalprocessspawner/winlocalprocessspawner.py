670b6a3d84a65124023ebf425003995130ff601f
smithed <smithed@users.noreply.github.com>
2018-11-08 19:30:30 -0800

    for null login token, change root user path to public
    The original behavior left the default, which is to write to c\users\default -- this is not a writable location, and so the hub is useless. (I found this by testing against the TmpAuthenticator). It may not be ideal, but changing it to public documents at least makes it usable. 
    
    I'm less familiar with python so I opted for more checks to avoid exceptions. There is probably a clearer way to write the change.

--- winlocalprocessspawner/winlocalprocessspawner.py
+++ winlocalprocessspawner/winlocalprocessspawner.py
@@ -78,6 +78,14 @@ class WinLocalProcessSpawner(LocalProcessSpawner):
             else:
                 # If the user profile is loaded, adjust APPDATA so the jupyter runtime files are stored
                 # in a per-user location.
+                
+                #If the token is None, the USERPROFILE points at the default directory which is not writable.
+                #this changes the path over to public documents, so at least its a writable location.
+                if 'USERPROFILE' in user_env:
+                    if user_env['USERPROFILE'].lower() == 'c:\\users\\default':
+                        if 'PUBLIC' in user_env:
+                            user_env['USERPROFILE'] = user_env['PUBLIC'] 
+                
                 if 'APPDATA' in user_env:
                     env['APPDATA'] = user_env['APPDATA']
                     env['USERPROFILE'] = user_env['USERPROFILE']
