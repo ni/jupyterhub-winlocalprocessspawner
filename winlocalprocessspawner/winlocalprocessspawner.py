18bdd0e8bbfa1d965cf34c831dd15ec3936e0279
ds <daniel_e_smith@outlook.com>
2018-11-12 20:09:20 -0800

    add new function which uses the jupyter cfg file to determine a suitable spawn cmd

--- winlocalprocessspawner/winlocalprocessspawner.py
+++ winlocalprocessspawner/winlocalprocessspawner.py
@@ -35,6 +35,48 @@ class WinLocalProcessSpawner(LocalProcessSpawner):
             if key in os.environ:
                 env[key] = os.environ[key]
         return env
+        
+        
+    def get_spawner_cmd(self):
+        """Using the config file, determine the absolute path of the script to run when spawning a new instance"""
+        #config: c.spawner.cmd
+        spawner_cmd=self.cmd
+        #this is just checking for empty settings, 
+        #which admittedly the parent probably fixes for you anyway
+        if spawner_cmd is None or spawner_cmd is '' or spawner_cmd is []:
+            spawner_cmd = 'jupyterhub-singleuser'
+            self.log.debug("no c.spawner.cmd specified, using default jupyterhub-singleuser")
+        
+        #jupyter docs say c.spawner.cmd is allowed to be a list or a string, 
+        #->convert it into a single path string
+        if type(spawner_cmd) is list:
+            #build path up
+            exe_cmd=''
+            for relpath in spawner_cmd:
+                exe_cmd=os.path.join(exe_cmd, relpath)
+        else:
+            exe_cmd=spawner_cmd
+        
+        #now we try to find the script because we need abs path to launch
+        if not os.path.exists(exe_cmd):
+            self.log.debug("cmd provided is not absolute or in the working directory, searching PATH")
+            for path_elem in os.environ['PATH'].split(os.pathsep):
+                #create an absolute path using the PATH var and see if it exists
+                checkpath=os.path.join(path_elem, exe_cmd)
+                if os.path.exists(checkpath):
+                    self.log.debug("cmd found on PATH at %s", path_elem)
+                    #this is the full abs path we've been looking for
+                    exe_cmd=checkpath
+                    break
+            else:
+                self.log.warning("cmd not found on path or in working directory, this will likely fail")
+        else:
+            exe_cmd=os.path.abspath(exe_cmd) #does nothing if already abs
+        
+        #we now have our abs path, add it to the launch cmd set
+        self.log.debug("Spawner will execute: %s", exe_cmd)
+        
+        return exe_cmd
 
     async def start(self):
         """Start the single-user server."""
@@ -44,14 +86,8 @@ class WinLocalProcessSpawner(LocalProcessSpawner):
         token = None
 
         cmd.append(sys.executable)
-        #in virtualenv, exe is in Scripts folder already vs main environment where exe is a python root
-        if hasattr(sys, 'real_prefix'):
-            #in virtual env
-            py_scripts_dir = os.path.dirname(sys.executable)
-        else:
-            #in main env
-            py_scripts_dir = os.path.join(os.path.dirname(sys.executable), 'Scripts')
-        cmd.append(os.path.join(py_scripts_dir, "jupyterhub-singleuser"))
+        
+        cmd.append(self.get_spawner_cmd())
 
         cmd.extend(self.get_args())
 
